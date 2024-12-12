2f2e9beb6e40e6599d55ef43df220acd3991fe7f
Ashley Willard <ashley.willard@gusto.com>
2024-12-12 15:31:55 -0800

    Add README template pathname
    We want to allow users to specify a README template other than the default template currently defined in `packs`. Users can specify an alternative path to this file in `packs.yml` or define `README_TEMPLATE.md` in the project root.

--- lib/packs/specification/configuration.rb
+++ lib/packs/specification/configuration.rb
@@ -10,15 +10,18 @@ module Packs
                                    'packs/*',
                                    'packs/*/*'
                                  ], T::Array[String])
+      DEFAULT_README_TEMPLATE_PATHNAME = T.let(Pathname.new('README_TEMPLATE.md'), Pathname)
 
       prop :pack_paths, T::Array[String]
+      prop :readme_template_pathname, Pathname
 
       sig { returns(Configuration) }
       def self.fetch
         config_hash = CONFIGURATION_PATHNAME.exist? ? YAML.load_file(CONFIGURATION_PATHNAME) : {}
 
         new(
-          pack_paths: pack_paths(config_hash)
+          pack_paths: pack_paths(config_hash),
+          readme_template_pathname: readme_template_pathname(config_hash)
         )
       end
 
@@ -29,6 +32,16 @@ module Packs
           DEFAULT_PACK_PATHS.dup
         else
           Array(specified_pack_paths)
+        end
+      end
+
+      sig { params(config_hash: T::Hash[T.untyped, T.untyped]).returns(Pathname) }
+      def self.readme_template_pathname(config_hash)
+        specified_readme_template_path = config_hash['readme_template_path']
+        if specified_readme_template_path.nil?
+          DEFAULT_README_TEMPLATE_PATHNAME
+        else
+          Pathname.new(specified_readme_template_path)
         end
       end
     end
